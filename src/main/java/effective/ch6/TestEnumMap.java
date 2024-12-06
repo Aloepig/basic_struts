d0b04902cd0ed8bbcacef6559c6fdb6732faa879
aloe <aloepig@gmail.com>
2024-12-06 17:50:46 +0900

    6장 이해하기

--- src/main/java/effective/ch6/TestEnumMap.java
+++ src/main/java/effective/ch6/TestEnumMap.java
@@ -50,7 +50,11 @@ public class TestEnumMap {
             }
 
             private static final Map<Phase, Map<Phase, Transition>> m
-                    = Stream.of(values()).collect(groupingBy(t -> t.from, () -> new EnumMap<>(Phase.class), toMap(t -> t.to, t -> t, (x, y) -> y, () -> new EnumMap<>(Phase.class))));
+                    = Stream.of(values()).collect(
+                    groupingBy(t -> t.from,
+                            () -> new EnumMap<>(Phase.class),
+                            toMap(t -> t.to, t -> t, (x, y) -> y, () -> new EnumMap<>(Phase.class))
+                    ));
 
             public static Transition get(Phase from, Phase to) {
                 return m.get(from).get(to);
