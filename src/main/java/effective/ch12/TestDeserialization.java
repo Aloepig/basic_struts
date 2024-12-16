d4dc36f1d285d6188dc51756ae074c72b5a5b898
aloe <aloepig@gmail.com>
2024-12-16 09:42:24 +0900

    주석 fixed

--- src/main/java/effective/ch12/TestDeserialization.java
+++ src/main/java/effective/ch12/TestDeserialization.java
@@ -2,17 +2,20 @@ package effective.ch12;
 
 import org.junit.jupiter.api.Test;
 
-import java.io.*;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
 import java.util.HashSet;
 import java.util.Set;
 
 public class TestDeserialization {
+
     @Test
     public void bombTest() throws IOException, ClassNotFoundException {
         byte[] serialized = bomb();
         // System.out.println(serialized);
-       // Set<Object> deserialize = (Set<Object>) new ObjectInputStream(new ByteArrayInputStream(serialized)).readObject();
-       // System.out.println(deserialize);
+        // Set<Object> deserialize = (Set<Object>) new ObjectInputStream(new ByteArrayInputStream(serialized)).readObject();
+        // System.out.println(deserialize);
     }
 
     private byte[] bomb() throws IOException {
@@ -20,17 +23,19 @@ public class TestDeserialization {
         Set<Object> s1 = root;
         Set<Object> s2 = new HashSet<>();
 
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 4; i++) {
             Set<Object> t1 = new HashSet<>();
-            Set<Object> t2 = new HashSet<>();
-            t1.add("foo" +i);
+            //Set<Object> t2 = new HashSet<>();
+            t1.add("foo" + i);
+
+            //System.out.println(i + "S:" + s1);
+            //System.err.println(i + "S:" + root);
 
             s1.add(t1);
             System.out.println(i + ":" + s1);
             System.err.println(i + ":" + root);
 
-            s1.add(t2);
-
+            // s1.add(t2);
             // s2.add(t1);
             // s2.add(t2);
 
